fe75aa8f51b03c5fa16f30f131c19e2a69797af5
fnormand <fnormand@sodius.com>
2019-01-14 11:54:25 +0100

    core: update for some testing regarding deep linking and SSR

--- apps/gubal/src/app/app.server.module.ts
+++ apps/gubal/src/app/app.server.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    FlexLayoutServerModule
+    FlexLayoutServerModule,
+    ModuleMapLoaderModule,
+    ServerTransferStateModule
   ],
   bootstrap: [AppComponent],
 })
